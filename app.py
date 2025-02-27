edac738ab6b2c5318da97524729bfbaf4839b1a9
Chioma Kamalu <kamaluchioma062@gmail.com>
2025-02-26 19:37:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -254,5 +254,6 @@ def download_csv():
         return jsonify({"error": "File not found"}), 404
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     # socketio.run(app, debug=True)
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
